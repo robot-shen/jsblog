85c34a7db3a46031237626598155c3e0916c3294
js_home <jins.robot@qq.com>
2018-04-11 22:40:28 +0800

    2018.4.11

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,43 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+# class User(AbstractUser):
+#     """
+#     Users within the Django authentication system are represented by this
+#     model.
+#
+#     Username, password and email are required. Other fields are optional.
+#     """
+#     class Meta(AbstractUser.Meta):
+#         swappable = 'AUTH_USER_MODEL'
+
 class Category(models.Model):
+    #文章分类
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()#最后一次编辑时间
+    excerpt = models.CharField(max_length=200, blank=True)#摘要
+    category = models.ForeignKey("Category")
+    tags = models.ManyToManyField("Tag",blank=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import HttpResponse
+from blog import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+import pytz
 
 # Create your views here.
+
+
+def test(request):
+    user = User.objects.get(id=1)
+    c = models.Category.objects.get(id=1)
+
+    # p = models.Article(title='title test', body='body test',created_time=timezone.now(), modified_time=timezone.now(),
+    #                    category=c, author=user)
+    p = models.Article.objects.filter(id=1).values()[0]
+    print(p['created_time'].strftime("%Y-%m-%d %H:%M:%S %Z"))
+    return HttpResponse(000)
